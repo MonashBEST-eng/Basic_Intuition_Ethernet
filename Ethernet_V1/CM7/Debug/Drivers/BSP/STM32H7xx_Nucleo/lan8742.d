e51211e0c9cbb29b4d01dff0f5470e04eef7de05
YoMoM829 <AnandVannalath@gmail.com>
2026-02-27 14:13:48 +1100

    Basic Ethernet code
    Added basic ethernet code

---
+++ Ethernet_V1/CM7/Debug/Drivers/BSP/STM32H7xx_Nucleo/lan8742.d
@@ -0,0 +1,4 @@
+Drivers/BSP/STM32H7xx_Nucleo/lan8742.o: \
+ C:/Users/ngocn/OneDrive\ -\ Monash\ University/MBEST/Ethernet_V1/Drivers/BSP/Components/lan8742/lan8742.c \
+ C:/Users/ngocn/OneDrive\ -\ Monash\ University/MBEST/Ethernet_V1/Drivers/BSP/Components/lan8742/lan8742.h
+C:/Users/ngocn/OneDrive\ -\ Monash\ University/MBEST/Ethernet_V1/Drivers/BSP/Components/lan8742/lan8742.h:
